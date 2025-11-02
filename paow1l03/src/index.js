9ac2328cb3e3a64c1fb1843266c65bf27002cd1d
Vi <107654718+fifth-monger@users.noreply.github.com>
2025-11-02 12:14:27 -0600

    learning about select elements

---
+++ paow1l03/src/index.js
@@ -0,0 +1,16 @@
+function showSelectedColor(event) {
+  if (event.target.value.length > 0) {
+    if (event.target.value === "red") {
+      alert("❤");
+    }
+    if (event.target.value === "blue") {
+      alert("💙");
+    }
+    if (event.target.value === "yellow") {
+      alert("💛");
+    }
+  }
+}
+
+let colorSelect = document.querySelector("#colors");
+colorSelect.addEventListener("change", showSelectedColor);
